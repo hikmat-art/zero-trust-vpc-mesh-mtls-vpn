a2e17261debc9988b788aa70106e2e74e344e50b
hikmat-art <itcombat97@gmail.com>
2025-12-29 09:42:19 +0500

    Update variables.tf

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,5 +1,29 @@
-// Network module variables
+variable "project_name" {
+  type        = string
+  description = "Project name prefix for resources"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (dev/prod)"
+}
+
 variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+  type        = string
+  description = "CIDR block for the VPC"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "List of public subnet CIDRs"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "List of private subnet CIDRs"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones to use"
 }
