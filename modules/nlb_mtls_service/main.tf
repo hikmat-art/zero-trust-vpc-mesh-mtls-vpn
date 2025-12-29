c82e1eaecc5a0a195e3a7c92ec689c9670ad42cd
hikmat-art <itcombat97@gmail.com>
2025-12-29 10:26:41 +0500

    Update main.tf

--- modules/nlb_mtls_service/main.tf
+++ modules/nlb_mtls_service/main.tf
@@ -1,2 +1,49 @@
-// NLB mTLS service module main.tf
-resource "null_resource" "nlb_placeholder" {}
+resource "aws_lb" "this" {
+  name               = "${var.project_name}-${var.environment}-nlb"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = var.public_subnet_ids
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-nlb"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_target_group" "this" {
+  name        = "${var.project_name}-${var.environment}-tg"
+  port        = var.target_port
+  protocol    = "TCP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+
+  health_check {
+    protocol = "TCP"
+    port     = var.target_port
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-tg"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_listener" "tls" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 443
+  protocol          = "TLS"
+
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-Ext-2018-06"
+  certificate_arn   = var.server_cert_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
+
+# Enable client certificate validation (mTLS)
+resource "aws_lb_listener_certificate" "client_auth" {
+  listener_arn    = aws_lb_listener.tls.arn
+  certificate_arn = var.client_ca_cert_arn
+}
