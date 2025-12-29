acfceb12d7573f9f3090e5d83672a33b4593d5fa
hikmat-art <itcombat97@gmail.com>
2025-12-29 10:27:20 +0500

    Update variables.tf

--- modules/nlb_mtls_service/variables.tf
+++ modules/nlb_mtls_service/variables.tf
@@ -1,4 +1,35 @@
-// NLB mTLS variables
-variable "service_name" {
-  type = string
+variable "project_name" {
+  type        = string
+  description = "Project name prefix"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (dev/prod)"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID where NLB is deployed"
+}
+
+variable "public_subnet_ids" {
+  type        = list(string)
+  description = "List of public subnet IDs"
+}
+
+variable "target_port" {
+  type        = number
+  description = "Port for target group"
+  default     = 8080
+}
+
+variable "server_cert_arn" {
+  type        = string
+  description = "ARN of server certificate issued by ACM PCA"
+}
+
+variable "client_ca_cert_arn" {
+  type        = string
+  description = "ARN of client CA certificate for mTLS validation"
 }
