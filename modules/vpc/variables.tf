6d651ce725cec6bbbe659b6a89444cd4dc4bcfba
hikmat-art <itcombat97@gmail.com>
2025-12-29 21:47:53 +0500

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+﻿###variable "name" {
+  type = string
+}
+
+variable "cidr_block" {
+  description = "Primary CIDR block for the VPC"
+  type        = string
+}
+
+variable "tags" {
+  description = "Map of tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
