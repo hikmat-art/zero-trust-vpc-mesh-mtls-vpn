4de746f18ba6752bf0eb28ad6301a9721802d8c5
hikmat-art <itcombat97@gmail.com>
2025-12-29 21:35:14 +0500

    Update backend.tf

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,3 +1,9 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "prod/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
