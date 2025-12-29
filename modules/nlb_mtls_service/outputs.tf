01123eddcf4d3be6df33c57590229a786ced96e8
hikmat-art <itcombat97@gmail.com>
2025-12-29 10:28:51 +0500

    Update outputs.tf

--- modules/nlb_mtls_service/outputs.tf
+++ modules/nlb_mtls_service/outputs.tf
@@ -1,4 +1,7 @@
-// NLB mTLS outputs
-output "nlb_dns" {
-  value = ""
+output "nlb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.this.arn
 }
