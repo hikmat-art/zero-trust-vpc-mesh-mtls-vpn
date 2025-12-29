0b204701995aca8608af10b0810c4cc5b9d31a5a
hikmat-art <itcombat97@gmail.com>
2025-12-29 21:24:05 +0500

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,2 +1,58 @@
-// IAM module main.tf
-resource "null_resource" "iam_placeholder" {}
+# Terraform execution role (example)
+resource "aws_iam_role" "terraform_role" {
+  name = "${var.project_name}-${var.environment}-terraform-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-terraform-role"
+    Environment = var.environment
+  }
+}
+
+# Scoped policy for Terraform-managed resources
+resource "aws_iam_role_policy" "terraform_policy" {
+  role = aws_iam_role.terraform_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ec2:Describe*",
+          "ec2:CreateVpc",
+          "ec2:DeleteVpc",
+          "ec2:CreateSubnet",
+          "ec2:DeleteSubnet",
+          "ec2:CreateInternetGateway",
+          "ec2:DeleteInternetGateway",
+          "ec2:CreateRouteTable",
+          "ec2:DeleteRouteTable",
+          "ec2:AssociateRouteTable",
+          "ec2:DisassociateRouteTable",
+          "ec2:CreateNatGateway",
+          "ec2:DeleteNatGateway",
+          "acm-pca:*",
+          "acm:*",
+          "elasticloadbalancing:*",
+          "logs:*",
+          "config:*",
+          "route53:*",
+          "iam:GetRole",
+          "iam:PassRole"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
