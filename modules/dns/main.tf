f42da91420a284b09b53f92a2ff2bcb7f066225e
hikmat-art <itcombat97@gmail.com>
2025-12-29 10:39:17 +0500

    Update main.tf

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -1,2 +1,25 @@
-// DNS module main.tf
-resource "null_resource" "dns_placeholder" {}
+resource "aws_route53_zone" "private" {
+  name          = var.domain_name
+  vpc {
+    vpc_id = var.vpc_id
+  }
+  comment       = "${var.project_name}-${var.environment}-private-zone"
+  force_destroy = true
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-dns"
+    Environment = var.environment
+  }
+}
+
+# Example record for NLB service
+resource "aws_route53_record" "service" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "${var.service_name}.${var.domain_name}"
+  type    = "A"
+  alias {
+    name                   = var.nlb_dns_name
+    zone_id                = var.nlb_zone_id
+    evaluate_target_health = true
+  }
+}
