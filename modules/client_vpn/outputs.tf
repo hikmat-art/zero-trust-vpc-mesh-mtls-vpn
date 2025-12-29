e0ed16a14731533d54e9bca98e2ddc83be9c99db
hikmat-art <itcombat97@gmail.com>
2025-12-29 10:37:03 +0500

    Update outputs.tf

--- modules/client_vpn/outputs.tf
+++ modules/client_vpn/outputs.tf
@@ -1,4 +1,7 @@
-// Client VPN outputs
-output "vpn_endpoint" {
-  value = ""
+output "client_vpn_endpoint_id" {
+  value = aws_ec2_client_vpn_endpoint.this.id
+}
+
+output "client_vpn_dns_name" {
+  value = aws_ec2_client_vpn_endpoint.this.dns_name
 }
