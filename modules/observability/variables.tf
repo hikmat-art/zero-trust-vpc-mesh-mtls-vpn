da5d8275744984201ef5ccb52dc64b519c2c23be
hikmat-art <itcombat97@gmail.com>
2025-12-29 21:20:41 +0500

    Update variables.tf

--- modules/observability/variables.tf
+++ modules/observability/variables.tf
@@ -1,4 +1,19 @@
-// Observability variables
-variable "log_group_name" {
-  type = string
+variable "project_name" {
+  type        = string
+  description = "Project name prefix"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (dev/prod)"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID for flow logs"
+}
+
+variable "config_bucket_name" {
+  type        = string
+  description = "S3 bucket name for AWS Config delivery"
 }
