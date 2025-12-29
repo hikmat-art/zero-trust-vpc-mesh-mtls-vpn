e07c5f91824778e0ad4899f5a001f85f5b56ee77
hikmat-art <itcombat97@gmail.com>
2025-12-29 21:21:16 +0500

    Update outputs.tf

--- modules/observability/outputs.tf
+++ modules/observability/outputs.tf
@@ -1,4 +1,7 @@
-// Observability outputs
-output "log_group_arn" {
-  value = ""
+output "flow_logs_log_group" {
+  value = aws_cloudwatch_log_group.vpc_flow_logs.name
+}
+
+output "config_recorder_name" {
+  value = aws_config_configuration_recorder.this.name
 }
