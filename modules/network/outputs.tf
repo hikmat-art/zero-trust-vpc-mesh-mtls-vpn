c92f2b53522fa4267b98eb06bf3498a38c1edb13
hikmat-art <itcombat97@gmail.com>
2025-12-29 09:42:55 +0500

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,4 +1,15 @@
-// Network outputs
 output "vpc_id" {
-  value = ""
+  value = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "nat_gateway_ids" {
+  value = aws_nat_gateway.this[*].id
 }
