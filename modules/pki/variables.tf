012c5f2982fb5cd2c6de7c6f9603abdff28d8066
hikmat-art <itcombat97@gmail.com>
2025-12-29 10:22:39 +0500

    Update variables.tf

--- modules/pki/variables.tf
+++ modules/pki/variables.tf
@@ -1,4 +1,25 @@
-// PKI variables
-variable "ca_name" {
-  type = string
+variable "project_name" {
+  type        = string
+  description = "Project name prefix"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (dev/prod)"
+}
+
+variable "root_ca_common_name" {
+  type        = string
+  description = "Common name for the root CA"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "Base domain name for issued certs"
+}
+
+variable "service_name" {
+  type        = string
+  description = "Service name for issued certificate"
+  default     = "app"
 }
