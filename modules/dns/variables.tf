e5bb1bfca3ae6954a24d096986e3c55503a90d57
hikmat-art <itcombat97@gmail.com>
2025-12-29 10:39:52 +0500

    Update variables.tf

--- modules/dns/variables.tf
+++ modules/dns/variables.tf
@@ -1,4 +1,35 @@
-// DNS variables
-variable "zone_name" {
-  type = string
+variable "project_name" {
+  type        = string
+  description = "Project name prefix"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (dev/prod)"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID to associate with private hosted zone"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "Base domain name for private DNS"
+}
+
+variable "service_name" {
+  type        = string
+  description = "Service name for DNS record"
+  default     = "app"
+}
+
+variable "nlb_dns_name" {
+  type        = string
+  description = "DNS name of the NLB"
+}
+
+variable "nlb_zone_id" {
+  type        = string
+  description = "Hosted zone ID of the NLB"
 }
