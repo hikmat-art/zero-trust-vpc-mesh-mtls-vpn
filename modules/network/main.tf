f108e8e858f1060fb27d70aee16bff918588dd2c
hikmat-art <itcombat97@gmail.com>
2025-12-29 09:40:54 +0500

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,3 +1,100 @@
-// Network module main.tf
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-vpc"
+    Environment = var.environment
+  }
+}
 
-resource "null_resource" "placeholder" {}
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-igw"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = element(var.availability_zones, count.index)
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-public-${count.index}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = element(var.availability_zones, count.index)
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-private-${count.index}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  count         = length(var.public_subnets)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-nat-${count.index}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_eip" "nat" {
+  count = length(var.public_subnets)
+  vpc   = true
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-eip-${count.index}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-public-rt"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-private-rt"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route" "private_nat" {
+  count                  = length(var.private_subnets)
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.this[*].id, count.index % length(aws_nat_gateway.this))
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
