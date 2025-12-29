8d5f492c75f662e6d78ee60138b3c42977614469
hikmat-art <itcombat97@gmail.com>
2025-12-29 21:19:59 +0500

    Update main.tf

--- modules/observability/main.tf
+++ modules/observability/main.tf
@@ -1,2 +1,96 @@
-// Observability module main.tf
-resource "null_resource" "obs_placeholder" {}
+# VPC Flow Logs
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/aws/vpc/${var.project_name}-${var.environment}-flowlogs"
+  retention_in_days = 30
+}
+
+resource "aws_flow_log" "this" {
+  log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  log_destination_type = "cloud-watch-logs"
+  traffic_type         = "ALL"
+  vpc_id               = var.vpc_id
+  iam_role_arn         = aws_iam_role.flow_logs_role.arn
+}
+
+resource "aws_iam_role" "flow_logs_role" {
+  name = "${var.project_name}-${var.environment}-flowlogs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "vpc-flow-logs.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "flow_logs_policy" {
+  role = aws_iam_role.flow_logs_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+# AWS Config for drift detection
+resource "aws_config_configuration_recorder" "this" {
+  name     = "${var.project_name}-${var.environment}-recorder"
+  role_arn = aws_iam_role.config_role.arn
+}
+
+resource "aws_config_delivery_channel" "this" {
+  name           = "${var.project_name}-${var.environment}-channel"
+  s3_bucket_name = var.config_bucket_name
+}
+
+resource "aws_config_configuration_recorder_status" "this" {
+  name    = aws_config_configuration_recorder.this.name
+  is_enabled = true
+}
+
+resource "aws_iam_role" "config_role" {
+  name = "${var.project_name}-${var.environment}-config-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "config.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "config_policy" {
+  role = aws_iam_role.config_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = [
+        "s3:PutObject",
+        "s3:GetBucketAcl",
+        "s3:GetBucketLocation",
+        "sns:Publish"
+      ]
+      Resource = "*"
+    }]
+  })
+}
