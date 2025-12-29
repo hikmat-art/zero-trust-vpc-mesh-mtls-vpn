6342caf925c6d7356250bb64e2b558a06e5a30a6
hikmat-art <itcombat97@gmail.com>
2025-12-29 10:36:21 +0500

    Update variables.tf

--- modules/client_vpn/variables.tf
+++ modules/client_vpn/variables.tf
@@ -1,4 +1,34 @@
-// Client VPN variables
-variable "client_cidr" {
-  type = string
+variable "project_name" {
+  type        = string
+  description = "Project name prefix"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (dev/prod)"
+}
+
+variable "vpn_cidr" {
+  type        = string
+  description = "CIDR block for client VPN"
+}
+
+variable "server_cert_arn" {
+  type        = string
+  description = "ARN of server certificate for VPN endpoint"
+}
+
+variable "client_ca_cert_arn" {
+  type        = string
+  description = "ARN of client CA certificate for authentication"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "List of subnet IDs to associate with VPN"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block of the VPC"
 }
