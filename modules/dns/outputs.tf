63cc086603a24e1b893d2b2b930bc4165a528d68
hikmat-art <itcombat97@gmail.com>
2025-12-29 10:40:25 +0500

    Update outputs.tf

--- modules/dns/outputs.tf
+++ modules/dns/outputs.tf
@@ -1,4 +1,7 @@
-// DNS outputs
-output "zone_id" {
-  value = ""
+output "private_zone_id" {
+  value = aws_route53_zone.private.zone_id
+}
+
+output "service_record_fqdn" {
+  value = aws_route53_record.service.fqdn
 }
