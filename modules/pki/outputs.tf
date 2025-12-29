a1fac36c367fa9fc1bdeed8fed64fdcbad12473a
hikmat-art <itcombat97@gmail.com>
2025-12-29 10:23:17 +0500

    Update outputs.tf

--- modules/pki/outputs.tf
+++ modules/pki/outputs.tf
@@ -1,4 +1,11 @@
-// PKI outputs
-output "ca_arn" {
-  value = ""
+output "root_ca_arn" {
+  value = aws_acmpca_certificate_authority.root.arn
+}
+
+output "intermediate_ca_arn" {
+  value = aws_acmpca_certificate_authority.intermediate.arn
+}
+
+output "service_cert_arn" {
+  value = aws_acm_certificate.service_cert.arn
 }
