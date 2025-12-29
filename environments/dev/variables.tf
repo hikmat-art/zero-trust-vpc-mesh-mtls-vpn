fd7c4ff41ecf96fbb029164fb8cf7f1f117c6d9e
hikmat-art <itcombat97@gmail.com>
2025-12-29 21:29:33 +0500

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,5 +1,59 @@
-// Dev variables
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name prefix"
+}
+
 variable "environment" {
-  type    = string
-  default = "dev"
+  type        = string
+  description = "Environment name (dev/prod)"
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR block for VPC"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "Public subnet CIDRs"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "Private subnet CIDRs"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "Availability zones"
+}
+
+variable "root_ca_common_name" {
+  type        = string
+  description = "Common name for root CA"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "Base domain name"
+}
+
+variable "service_name" {
+  type        = string
+  description = "Service name for DNS and certs"
+}
+
+variable "vpn_cidr" {
+  type        = string
+  description = "CIDR block for VPN clients"
+}
+
+variable "config_bucket_name" {
+  type        = string
+  description = "S3 bucket for AWS Config delivery"
 }
