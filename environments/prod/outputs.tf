be2ad8de9f111a4f39c87724a85fee48b24fb220
hikmat-art <itcombat97@gmail.com>
2025-12-29 21:34:28 +0500

    Update outputs.tf

--- environments/prod/outputs.tf
+++ environments/prod/outputs.tf
@@ -1,4 +1,15 @@
-// Prod outputs
-output "env" {
-  value = "prod"
+output "vpc_id" {
+  value = module.network.vpc_id
+}
+
+output "nlb_dns_name" {
+  value = module.nlb_mtls_service.nlb_dns_name
+}
+
+output "client_vpn_endpoint_id" {
+  value = module.client_vpn.client_vpn_endpoint_id
+}
+
+output "service_record_fqdn" {
+  value = module.dns.service_record_fqdn
 }
